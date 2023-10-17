bccdaade7ee1a5d82373c53914c1ab9400d8bc29
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-10-17 21:44:37 +0200

    feat: update Profile.jsx with css-modules

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,1 +1,28 @@
 import Proptypes from 'prop-types';
+import css from './Profile.module.css';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  <div className={css.profile}>
+    <div className={css.description}>
+      <img src={avatar} alt="User avatar" className={css.avatar} />
+      <p class="name">Petra Marica</p>
+      <p class="tag">@pmarica</p>
+      <p class="location">Salvador, Brasil</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">1000</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">2000</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">3000</span>
+      </li>
+    </ul>
+  </div>;
+};
