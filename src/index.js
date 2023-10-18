5d5a52f1b82d1ff47840e1016b3fed8ea35b71c3
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-10-18 18:41:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import './index.css';
 import { Profile } from 'components/Profile/Profile';
 import user from './components/Profile/user.json';
 
+// import { Statistics } from 'components/Statistics/Statistics';
+// import data from './components/Statistics/data.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
@@ -16,6 +19,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} /> */}
     </App>
   </React.StrictMode>
 );
