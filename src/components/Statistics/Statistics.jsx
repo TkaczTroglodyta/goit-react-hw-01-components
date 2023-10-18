742279973949fa5de997f7264908792ce9c10ee9
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-10-18 20:44:34 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,3 +4,8 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => (
   <section className={css.statistics}></section>
 );
+
+function getRandomHexColor() {
+  let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return bgColor;
+}
