512fd7bc87cfea791edd8b5f946d606bff9fafb3
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-10-18 21:47:07 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -6,11 +6,29 @@ export const Statistics = ({ title, stats }) => (
     {title && <h2 className={css.title}>{title}</h2>}
     <ul className={css.statList}>
       {stats.map(stat => (
-        <li className={css.statList}></li>
+        <li
+          className={title ? css.statListTitle : css.statListNoTitle}
+          style={{ backgroundColor: getRandomHexColor() }}
+          key={stat.id}
+        >
+          <span className={css.label}>{stat.label}</span>
+          <span className={css.percentage}>{stat.percentage}%</span>
+        </li>
       ))}
     </ul>
   </section>
 );
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
 
 function getRandomHexColor() {
   let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
