2ea64aa773afb62b6b8b65c7a0560751a7b2c5eb
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-10-18 21:20:51 +0200

    feat: update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,14 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => (
-  <section className={css.statistics}></section>
+  <section className={css.statistics}>
+    {title && <h2 className={css.title}>{title}</h2>}
+    <ul className={css.statList}>
+      {stats.map(stat => (
+        <li className={css.statList}></li>
+      ))}
+    </ul>
+  </section>
 );
 
 function getRandomHexColor() {
