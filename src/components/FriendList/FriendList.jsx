0ea71b969a7d9604d00e6922b4595cc108eded71
Andrzej Janiak <andrzej.janiak@outlook.com>
2023-10-19 20:52:30 +0200

    Added <ul> with FriendListItem

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => (
+  <ul className={css.friendList}>
+    {friends.map(friend => (
+      <FriendListItem
+        key={friend.id}
+        avatar={friend.avatar}
+        isOnline={friend.isOnline}
+        name={friend.name}
+      ></FriendListItem>
+    ))}
+  </ul>
+);
